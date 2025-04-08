135ef7c32ccf3382004b319dd0b871f08429675b
kapilan <166731523+kapilanramesh@users.noreply.github.com>
2025-04-08 14:21:53 +0530

    variable.tf
    variables for s3 bucket and dynamo DB

---
+++ variable.tf
@@ -0,0 +1,21 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "s3_bucket_name" {
+  description = "Name of the S3 bucket to store Terraform state"
+  type        = string
+}
+
+variable "dynamodb_table_name" {
+  description = "Name of the DynamoDB table for Terraform state locking"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name (e.g., dev, prod)"
+  type        = string
+  default     = "dev"
+}
